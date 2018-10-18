eed5bddd08db0e19409fbf51e6806583a939c74f
Luzi Schoeb <schoeb.luzi@gmail.com>
2018-10-18 21:35:01 +0200

    Keys  #15 (tested on Remote, not all SW) realised with Interrupts. Console with RTTClient

--- TEAM_Remote/Sources/Platform_Local.h
+++ TEAM_Remote/Sources/Platform_Local.h
@@ -16,13 +16,13 @@
 
 /* platform hardware configuration */
 #define PL_LOCAL_CONFIG_NOF_LEDS            (1) /* number of LEDs 0 to 3, Remote has 1 (PTD4) */
-#define PL_LOCAL_CONFIG_NOF_KEYS            (0) /* number of keys, 0 to 7 */
+#define PL_LOCAL_CONFIG_NOF_KEYS            (7) /* number of keys, 0 to 7 */
 
 #if PL_LOCAL_CONFIG_NOF_KEYS>0
-  #define PL_LOCAL_CONFIG_KEY_1_ISR         (0) /* if SW1 is using interrupts */
-  #define PL_LOCAL_CONFIG_KEY_2_ISR         (0) /* if SW2 is using interrupts */
+  #define PL_LOCAL_CONFIG_KEY_1_ISR         (1) /* if SW1 is using interrupts */
+  #define PL_LOCAL_CONFIG_KEY_2_ISR         (1) /* if SW2 is using interrupts */
   #define PL_LOCAL_CONFIG_KEY_3_ISR         (0) /* if SW3 is using interrupts */
-  #define PL_LOCAL_CONFIG_KEY_4_ISR         (0) /* if SW4 is using interrupts */
+  #define PL_LOCAL_CONFIG_KEY_4_ISR         (1) /* if SW4 is using interrupts */
   #define PL_LOCAL_CONFIG_KEY_5_ISR         (0) /* if SW5 is using interrupts */
   #define PL_LOCAL_CONFIG_KEY_6_ISR         (0) /* if SW6 is using interrupts */
   #define PL_LOCAL_CONFIG_KEY_7_ISR         (0) /* if SW7 is using interrupts */
@@ -32,13 +32,13 @@
 //#define PL_LOCAL_CONFIG_HAS_LEDS_DISABLED                 /* disable LEDs */
 //#define PL_LOCAL_CONFIG_HAS_EVENTS_DISABLED               /* disable events */
 //#define PL_LOCAL_CONFIG_HAS_TIMER_DISABLED                /* disable own timer */
-#define PL_LOCAL_CONFIG_HAS_KEYS_DISABLED                 /* disable key/push buttons */
-#define PL_LOCAL_CONFIG_HAS_SHELL_DISABLED                /* disable shell */
+//#define PL_LOCAL_CONFIG_HAS_KEYS_DISABLED                 /* disable key/push buttons */
+//#define PL_LOCAL_CONFIG_HAS_SHELL_DISABLED                /* disable shell */
 #define PL_LOCAL_CONFIG_HAS_TRIGGER_DISABLED              /* disable triggers */
 #define PL_LOCAL_CONFIG_HAS_DEBOUNCE_DISABLED             /* disable debouncing */
 #define PL_LOCAL_CONFIG_HAS_RTOS_DISABLED                 /* disable RTOS usage */
 #define PL_LOCAL_CONFIG_HAS_USB_CDC_DISABLED              /* disable USB CDC */
-#define PL_LOCAL_CONFIG_HAS_SEGGER_RTT_DISABLED           /* disable Segger RTT */
+//#define PL_LOCAL_CONFIG_HAS_SEGGER_RTT_DISABLED           /* disable Segger RTT */
 #define PL_LOCAL_CONFIG_HAS_SHELL_QUEUE_DISABLED          /* disable shell queue */
 #define PL_LOCAL_CONFIG_HAS_SQUEUE_SINGLE_CHAR_DISABLED   /* disable single character support in shell queue */
 #define PL_LOCAL_CONFIG_HAS_SEMAPHORE_DISABLED            /* disable semaphore test module */
